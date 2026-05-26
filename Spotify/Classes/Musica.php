cb2e543e3d2f76c6aa3aacd02f3bb23d75b77021
VNsdemo <viniciusicaro2077@gmail.com>
2026-05-26 20:46:16 -0300

    Reorder constructor parameters in Musica class

--- Spotify/Classes/Musica.php
+++ Spotify/Classes/Musica.php
@@ -4,17 +4,15 @@ declare(strict_types=1);
 
 require_once 'Midia.php';
 
-class Musica extends Midia
-{
-    public function __construct(
-        private string $artista,
-        private string $album,
-        private string $genero,
-        string $titulo,
-        int $duracao
-    ) {
-        parent::__construct($titulo, $duracao);
-    }
+public function __construct(
+    string $titulo,
+    int $duracao,
+    private string $artista,
+    private string $album,
+    private string $genero
+) {
+    parent::__construct($titulo, $duracao);
+}
 
     public function reproduzir(): string
     {
